4ad951bfa2827b62e70ad1023c406d698cdad6da
NathanRoland <nathanroland635@gmail.com>
2025-08-30 16:33:12 +1000

    interactive chat and analyse buttons that send data to the backendpoints

--- Extension Files/scripts/chat.js
+++ Extension Files/scripts/chat.js
@@ -1,1 +1,18 @@
 console.log("chat.js");
+
+const chatButton = document.getElementById('chat-button');
+const chatInput = document.getElementById('chat-input');
+
+chatButton.addEventListener('click', () => {
+    const product = chatInput.value;
+    console.log(product);
+    //this will be sent to the backend and response will be displayed in the chat container
+    const response = ("Response:this is a test response");
+    const chatPrompt = document.getElementById("chat-prompt");
+    chatPrompt.innerHTML = product;
+    console.log(response);
+    //display the response in the chat container
+    const chatContainer = document.getElementById("chat-container");
+    chatContainer.innerHTML = response;
+    
+});
